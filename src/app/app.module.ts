54c51ef2b4fc14c9fe3f97ca07a482c4b7a022ed
Davit Nikuradze <nikurzed@gmail.com>
2023-08-04 02:31:40 +0400

    some new things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent, NavbarComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    MapComponent,
+    NavbarComponent,
+    AboutPageComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  search() {
+    const searchTerm = (
+      document.querySelector('.search-bar input') as HTMLInputElement
+    ).value;
+    console.log('Searching for:', searchTerm);
+
+    window.location.reload();
+  }
+
+  constructor(private router: Router) {}
+
+  goToAboutPage() {
+    this.router.navigateByUrl('/about');
+  }
+}
