a4002d94c65338e5ae7a50e5b68707ebb8144ecc
Davit Nikuradze <nikurzed@gmail.com>
2023-08-04 04:41:24 +0400

    almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AboutComponent } from './about/about.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/map', pathMatch: 'full' },
+  { path: 'map', component: MapComponent },
+  { path: 'about', component: AboutPageComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, MapComponent, NavbarComponent, AboutComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    MapComponent,
+    NavbarComponent,
+    AboutPageComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
